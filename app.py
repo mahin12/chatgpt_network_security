e98ee9ff81ecabfbe91f3bcbb073b1eaacd7238a
Mahin12 <mahinarafatmeem@gmail.com>
2023-04-09 01:45:16 -0400

    Initial commit

--- app.py
+++ app.py
@@ -75,22 +75,27 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        cluster = MongoClient(
-            "mongodb+srv://asmafariha:access123@cluster0.t1qqadg.mongodb.net/?retryWrites=true&w=majority")
-        db = cluster["securifyGPT"]
-        collection = db["userinfo"]
-        results = collection.find({"_id": username})
-        for result in results:
-            if result["password"] == password:
-                user = User(1)
-                login_user(user)
-                session['username'] = username
-                return render_template('homepage.html', user=username)
-            else:
-                return render_template('login.html', error='Invalid username or password. Try Again.')
+        try:
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cluster = MongoClient(
+                "mongodb+srv://asmafariha:access123@cluster0.t1qqadg.mongodb.net/?retryWrites=true&w=majority")
+            db = cluster["securifyGPT"]
+            collection = db["userinfo"]
+            results = collection.find({"_id": username})
+            for result in results:
+                if result["password"] == password:
+                    user = User(1)
+                    login_user(user)
+                    session['username'] = username
+                    return render_template('homepage.html', user=username)
+                else:
+                    return render_template('login.html', error='Invalid username or password. Try Again.')
+        except Exception as e:
+            print(e)
+            return render_template('login.html', error='Internal Server Error')
     return render_template('login.html')
+
 
 
 @app.route('/testCode', methods=['POST'])
