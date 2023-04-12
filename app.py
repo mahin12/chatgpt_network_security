8dface4686c296591512e331b5c3ad0e84927666
Mahin12 <mahinarafatmeem@gmail.com>
2023-04-12 16:56:34 -0400

    Your commit message here

--- app.py
+++ app.py
@@ -1,61 +1,103 @@
-import requests
-from scapy.all import sniff
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
-import pymongo
-from pymongo import MongoClient
-import openai
-from flask import Flask, request, session
-import os
-from datetime import datetime
+# """
+# SecurifyGPT - A Flask web application for analyzing code security using OpenAI's GPT model.
 
+# This web application enables users to test code for security threats by leveraging OpenAI's GPT model.
+# Users can log in, upload code, and receive a security analysis report, which will also be stored in their search history.
+
+# Dependencies:
+# - Flask
+# - Flask-Login
+# - Requests
+# - OpenAI
+# - PyMongo
+# - Scapy
+# - MongoDB
+
+# Application Structure:
+# 1. Import dependencies
+# 2. Initialize Flask app, LoginManager, and User class for authentication
+# 3. Define routes and corresponding functions:
+#     a. index: root route for the main page
+#     b. website: route for the website cover
+#     c. home: route for the homepage
+#     d. test_your_code: route for POST request to test code
+#     e. test: route to render the test page
+#     f. access_history: route for POST request to access history
+#     g. login: route for login page and authentication
+#     h. logout: route for POST request to logout
+#     i. register: route for registration page
+#     j. upload_file: route for POST request to upload a file for analysis
+#     k. ask_chatgpt: route for POST request to analyze code using GPT
+# 4. Main function to run the Flask app
+
+# Note: Before running the application, replace 'your-secret-key' with your own secret key and
+# update the MongoDB connection string, API key, and API URL as necessary.
+# """
+
+
+
+
+import os
+import requests
+import openai
+import pymongo
+
+from datetime import datetime
+from pymongo import MongoClient
+from scapy.all import sniff
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+
+# Initialize the Flask app
 app = Flask(__name__)
 app.secret_key = 'your-secret-key'
 username = None
+
+# Initialize LoginManager
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-
+# User class for authentication
 class User(UserMixin):
     def __init__(self, id):
         self.id = id
 
-
+# Load user for authentication
 @login_manager.user_loader
 def load_user(user_id):
     return User(user_id)
 
-
+# Index route
 @app.route('/')
 def index():
     return render_template('webCover.html')
     # username = session['username']
     # eturn render_template('homePage.html', username=username)
 
-
+# Website route
 @app.route('/website')
 def website():
     return render_template('webCover.html')
 
-
+# Homepage route
 @app.route('/homepage')
 def home():
     return render_template('homePage.html', username=session['username'])
 
-
+# Test your code route (POST)
 @app.route('/testCode', methods=['POST'])
 @login_required
 def test_your_code():
     return redirect(url_for('test'))
 
-
+# Test route
 @app.route('/test')
 @login_required
 def test():
     return render_template('test.html')
 
-
+# Access history route (POST)
 @app.route('/history', methods=['POST'])
 @login_required
 def access_history():
@@ -70,9 +112,10 @@ def access_history():
 
     return render_template('historyPage.html', results=history)
 
-
+# Login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Authenticate user and create session
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
@@ -91,14 +134,14 @@ def login():
                 return render_template('login.html', error='Invalid username or password. Try Again.')
     return render_template('login.html')
 
-
+# Logout route (POST)
 @app.route('/logout', methods=['POST'])
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
-
+# Register route
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -124,11 +167,11 @@ def register():
         return render_template('login.html', message=message)
     return render_template('registration.html')
 
-
+# API key and URL
 API_KEY = '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'
 API_URL = 'https://api.openai.com/v1/engines/text-davinci-002/completions'
 
-
+# Upload file route (POST)
 @app.route('/upload', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
@@ -169,7 +212,7 @@ def upload_file():
     result = response.choices[0].text.strip()
     return jsonify({'response': result})
 
-
+# Ask ChatGPT route (POST)
 @app.route('/ask', methods=['POST'])
 @login_required
 def ask_chatgpt():
@@ -209,6 +252,6 @@ def ask_chatgpt():
     result = response.choices[0].text.strip()
     return jsonify({'response': result})
 
-
+# Main function
 if __name__ == '__main__':
     app.run(debug=True)
